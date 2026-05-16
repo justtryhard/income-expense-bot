d7cf81268508ddcecd5abc8d5ff72904adc9b795
justtryhard <lexchernobrov@gmail.com>
2026-05-16 12:56:29 +0300

    FSM cmntd

--- states.py
+++ states.py
@@ -1,18 +1,32 @@
 from aiogram.fsm.state import State, StatesGroup
 
+# FSM состояния приложения. Используется для пошагового общения с юзером
+
 
 class AddEntry(StatesGroup):
+    """
+    Состояния добавления операций
+    """
     waiting_for_amount = State()
 
 
 class StatsPeriod(StatesGroup):
+    """
+    Состояния выбора периода статистики
+    """
     waiting_for_start = State()
     waiting_for_end = State()
 
 
 class EditEntry(StatesGroup):
+    """
+    Состояния редактирования записи
+    """
     waiting_for_id = State()
 
 
 class EditAmount(StatesGroup):
+    """
+    Состояния изменения сумм
+    """
     waiting_for_amount = State()
