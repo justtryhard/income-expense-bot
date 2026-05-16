a2d32875ee6d38863b1e78024e20084e20d68cd9
justtryhard <lexchernobrov@gmail.com>
2026-05-16 12:44:39 +0300

    utils comm

--- utils/validators.py
+++ utils/validators.py
@@ -1,2 +1,6 @@
 def is_positive_int(value: str) -> bool:
+    """
+    Валидатор
+    Проверка, что строка состоит только из цифр и число положительное
+    """
     return value.isdigit() and int(value) > 0
