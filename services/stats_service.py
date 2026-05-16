3e325a37a1115ac8fe6ce76bdaee6835b7c0d5aa
justtryhard <lexchernobrov@gmail.com>
2026-05-16 12:38:14 +0300

    stats service commented

--- services/stats_service.py
+++ services/stats_service.py
@@ -1,9 +1,17 @@
 class StatsService:
+    """
+    Сервис статистики
+    Прослойка между хендлерами и репозиториями
+    Отвечает за получение статистики (в тч дневной), получение операций за день
+    """
     def __init__(self, stats_repository, transaction_repository):
         self.stats_repository = stats_repository
         self.transaction_repository = transaction_repository
 
     def get_period_stats(self, user_id: int, start_date, end_date):
+        """
+        Получение статистики за период
+        """
         return self.stats_repository.get_stats_for_period(
             user_id=user_id,
             start_date=start_date,
@@ -11,6 +19,9 @@ class StatsService:
         )
 
     def get_daily_stats(self, user_id: int, start_date, end_date):
+        """
+        Получение статистики по дням, используется для построения графика
+        """
         return self.stats_repository.get_daily_stats(
             user_id=user_id,
             start_date=start_date,
@@ -18,4 +29,7 @@ class StatsService:
         )
 
     def get_records_for_day(self, user_id: int, date):
+        """
+        Получение операций за конкретный день
+        """
         return self.transaction_repository.get_records_by_date(user_id, date)
