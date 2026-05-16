52738aa1cf246bb7ba2936d66e5f1ae0cc592a67
justtryhard <lexchernobrov@gmail.com>
2026-05-16 10:51:13 +0300

    database refactoring

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ load_dotenv()
 API_TOKEN = os.getenv("API_TOKEN")
 DB_NAME = os.getenv("DB_NAME", "finance.db")
 ALLOWED_USER_ID = int(os.getenv("ALLOWED_USER_ID"))
+
+if API_TOKEN is None:
+    raise ValueError("API_TOKEN не задан")
