e9a55ff04f8d91ca3c3ba6111983da52757ad2bc
justtryhard <lexchernobrov@gmail.com>
2026-05-16 12:44:39 +0300

    utils comm

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+# Работа с московским временем
+# Используется при создании записей в БД, чтобы серверное время не ломало даты
+# Даже если сервер находится в другом часовом поясе - запись всегда будет по МСК
 
 MOSCOW_TZ = ZoneInfo("Europe/Moscow")
 
